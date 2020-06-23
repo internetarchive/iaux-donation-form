7f38328499cf6b63f7317af2e995ad80a3de1e4c
Jason Buckner <jason@jasonbuckner.com>
2020-06-23 14:23:04 -0700

    Update exports

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,5 @@
-export { DonationForm } from './lib/donation-form.d.ts';
+export { DonationForm } from './lib/donation-form.d';
+export { BraintreeManager, BraintreeEndpointManagerInterface } from './lib/braintree-manager/braintree-manager.d';
+export { RecaptchaManager, RecaptchaManagerInterface } from './lib/recaptcha-manager/recaptcha-manager.d';
+export { PaymentFlowHandlers, PaymentFlowHandlersInterface } from './lib/payment-flow-handlers/payment-flow-handlers.d';
+export { PaymentClients, PaymentClientsInterface } from './lib/braintree-manager/payment-clients.d';
