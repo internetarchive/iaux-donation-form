b056c8fb25bfd6e6932945d357862f4de64378ff
Jason Buckner <jason@jasonbuckner.com>
2020-06-24 10:51:56 -0700

    Add additional exports

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 export { DonationForm } from './lib/donation-form.d';
-export { BraintreeManager, BraintreeEndpointManagerInterface } from './lib/braintree-manager/braintree-manager.d';
+export { BraintreeManager } from './lib/braintree-manager/braintree-manager.d';
+export { BraintreeManagerInterface, BraintreeEndpointManagerInterface } from './lib/braintree-manager/braintree-interfaces.d';
 export { RecaptchaManager, RecaptchaManagerInterface } from './lib/recaptcha-manager/recaptcha-manager.d';
 export { PaymentFlowHandlers, PaymentFlowHandlersInterface } from './lib/payment-flow-handlers/payment-flow-handlers.d';
 export { PaymentClients, PaymentClientsInterface } from './lib/braintree-manager/payment-clients.d';
