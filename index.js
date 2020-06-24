e3c5163e5e0d0e82ff67dc61167a24fbd49a8160
Jason Buckner <jason@jasonbuckner.com>
2020-06-24 10:31:43 -0700

    Fix PaymentClients export

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export { DonationForm } from './lib/donation-form.js';
 export { RecaptchaManager } from './lib/recaptcha-manager/recaptcha-manager.js';
 export { PaymentFlowHandlers } from './lib/payment-flow-handlers/payment-flow-handlers.js';
-export { PaymentClients } from './lib/payment-flow-handlers/payment-flow-handlers.js';
+export { PaymentClients } from './lib/braintree-manager/payment-clients.js';
 export { BraintreeManager } from './lib/braintree-manager/braintree-manager.js';
