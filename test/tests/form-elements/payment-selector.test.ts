35099e65642ecff9d23900b3396a4747bfb69450
Jason Buckner <jason@jasonbuckner.com>
2020-08-03 15:28:02 -0700

    Add some more payment selector tests

--- test/tests/form-elements/payment-selector.test.ts
+++ test/tests/form-elements/payment-selector.test.ts
@@ -1,4 +1,4 @@
-import { html, fixture, expect, elementUpdated } from '@open-wc/testing';
+import { html, fixture, expect, elementUpdated, oneEvent } from '@open-wc/testing';
 import { PaymentSelector } from '../../../src/form-elements/payment-selector';
 import '../../../src/form-elements/payment-selector';
 import { MockPaymentProviders } from '../../mocks/payment-providers/mock-payment-providers';
@@ -18,4 +18,50 @@ describe('Payment Selector', () => {
     const venmoButton = el.shadowRoot?.querySelector('.venmo.provider-button');
     expect(venmoButton?.classList.contains('available')).to.be.true;
   });
+
+  it('can show PayPal when called', async () => {
+    const el = (await fixture(html`
+      <payment-selector></payment-selector>
+    `)) as PaymentSelector;
+    el.showPaypalButton();
+    await elementUpdated(el);
+    const paypalButton = el.shadowRoot?.querySelector('.paypal-container.provider-button');
+    expect(paypalButton?.classList.contains('available')).to.be.true;
+  });
+
+  it('emits paypalBlockerSelected event when paypal is selected in an error state', async () => {
+    const el = (await fixture(html`
+      <payment-selector></payment-selector>
+    `)) as PaymentSelector;
+    const paypalBlocker = el.shadowRoot?.querySelector('.paypal-local-button');
+    const clickEvent = new MouseEvent('click');
+    setTimeout(() => { paypalBlocker?.dispatchEvent(clickEvent); });
+    const response = await oneEvent(el, 'paypalBlockerSelected');
+    expect(response).to.exist;
+  });
+
+  it('emits applePaySelected event with original click event when ApplePay is selected', async () => {
+    const el = (await fixture(html`
+      <payment-selector></payment-selector>
+    `)) as PaymentSelector;
+    const paypalBlocker = el.shadowRoot?.querySelector('.applepay.provider-button');
+    const clickEvent = new MouseEvent('click');
+    setTimeout(() => { paypalBlocker?.dispatchEvent(clickEvent); });
+    const response = await oneEvent(el, 'applePaySelected');
+    const event = response.detail.originalEvent;
+    expect(event).to.equal(clickEvent);
+  });
+
+  it('emits googlePaySelected when GooglePay is selected', async () => {
+    const el = (await fixture(html`
+      <payment-selector></payment-selector>
+    `)) as PaymentSelector;
+    const paypalBlocker = el.shadowRoot?.querySelector('.googlepay.provider-button');
+    const clickEvent = new MouseEvent('click');
+    setTimeout(() => { paypalBlocker?.dispatchEvent(clickEvent); });
+    const response = await oneEvent(el, 'googlePaySelected');
+    expect(response).to.exist;
+  });
+
+
 });
