1895bc5b7d920c905911db3c85d39280a7286623
Jason Buckner <jason@jasonbuckner.com>
2020-09-29 11:15:58 -0700

    Base config updates

--- packages/banner-thermometer/.eslintrc.js
+++ packages/banner-thermometer/.eslintrc.js
@@ -1,20 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import', 'html'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-  ],
-  rules: {
-    // disable the rule for all files
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'import/named': 'off',
-    'import/no-unresolved': 'off',
-    'import/extensions': ['error', 'always', { ignorePackages: true }],
+  plugins: ['@typescript-eslint', 'html'],
+  extends: ['plugin:@typescript-eslint/recommended'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src', 'demo'],
+      },
+    },
   },
 };
