10166c179189fc4d842f376a608bbe3e16b23a14
Jason Buckner <jasonb@archive.org>
2021-02-03 09:43:53 -0800

    Re-add Donation Form Address (#36)
    Re-add the donation form address fields. Previously we were only collecting name, email, and zip

--- packages/donation-form/src/form-elements/contact-form/contact-form.ts
+++ packages/donation-form/src/form-elements/contact-form/contact-form.ts
@@ -8,7 +8,7 @@ import {
   DonorContactInfo,
 } from '@internetarchive/donation-form-data-models';
 import { AutoCompleteFieldOptions } from './autocomplete-field-options';
-import { IconSpaceOption } from '../badged-input';
+import { SpacerOption } from '../badged-input';
 import { BadgedInput } from '../badged-input';
 import '../badged-input';
 
@@ -16,57 +16,68 @@ import emailImg from '@internetarchive/icon-email';
 import localePinImg from '@internetarchive/icon-locale-pin';
 import userIcon from '@internetarchive/icon-user';
 
+import { countries } from './countries';
+
 @customElement('contact-form')
 export class ContactForm extends LitElement {
-  @query('badged-input.email') emailBadgedInput!: BadgedInput;
-  @query('#email') emailField!: HTMLInputElement;
-  @query('badged-input.first-name') firstNameBadgedInput!: BadgedInput;
-  @query('#first-name') firstNameField!: HTMLInputElement;
-  @query('badged-input.last-name') lastNameBadgedInput!: BadgedInput;
-  @query('#last-name') lastNameField!: HTMLInputElement;
-  @query('badged-input.postal-code') postalBadgedInput!: BadgedInput;
-  @query('#postal-code') postalCodeField!: HTMLInputElement;
+  @query('badged-input.donation-contact-form-email') emailBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-email') emailField!: HTMLInputElement;
 
-  @query('#error-message') errorMessage!: HTMLDivElement;
+  @query('badged-input.donation-contact-form-first-name') firstNameBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-first-name') firstNameField!: HTMLInputElement;
+
+  @query('badged-input.donation-contact-form-last-name') lastNameBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-last-name') lastNameField!: HTMLInputElement;
+
+  @query('badged-input.donation-contact-form-postal-code') postalBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-postal-code') postalCodeField!: HTMLInputElement;
+
+  @query('badged-input.donation-contact-form-street-address')
+  streetAddressBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-street-address') streetAddressField!: HTMLInputElement;
+
+  @query('badged-input.donation-contact-form-extended-address')
+  extendedAddressBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-extended-address') extendedAddressField!: HTMLInputElement;
+
+  @query('badged-input.donation-contact-form-locality') localityBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-locality') localityField!: HTMLInputElement;
+
+  @query('badged-input.donation-contact-form-region') regionBadgedInput!: BadgedInput;
+  @query('#donation-contact-form-region') regionField!: HTMLInputElement;
+
+  @query('#donation-contact-form-countryCodeAlpha2') countryCodeAlpha2Field!: HTMLSelectElement;
+
+  @query('#donation-contact-form-error-message') errorMessage!: HTMLDivElement;
   @query('form') form!: HTMLFormElement;
 
   reportValidity(): boolean {
-    const emailValid = this.emailField.checkValidity();
-    if (!emailValid) {
-      this.emailBadgedInput.error = true;
-    }
+    const fieldBadgedInputs: Array<[HTMLInputElement, BadgedInput]> = [
+      [this.emailField, this.emailBadgedInput],
+      [this.firstNameField, this.firstNameBadgedInput],
+      [this.lastNameField, this.lastNameBadgedInput],
+      [this.regionField, this.regionBadgedInput],
+      [this.localityField, this.localityBadgedInput],
+      [this.streetAddressField, this.streetAddressBadgedInput],
+      [this.postalCodeField, this.postalBadgedInput],
+    ];
 
-    const firstNameValid = this.firstNameField.checkValidity();
-    if (!firstNameValid) {
-      this.firstNameBadgedInput.error = true;
-    }
+    let isValid = true;
+    fieldBadgedInputs.forEach(([inputElement, badgedInput]) => {
+      const fieldValid = inputElement.checkValidity();
+      isValid = isValid && fieldValid;
+      if (!fieldValid) {
+        badgedInput.error = true;
+      }
+    });
 
-    const lastNameValid = this.lastNameField.checkValidity();
-    if (!lastNameValid) {
-      this.lastNameBadgedInput.error = true;
-    }
-
-    const postalValid = this.postalCodeField.checkValidity();
-    if (!postalValid) {
-      this.postalBadgedInput.error = true;
-    }
-
-    const valid = emailValid && firstNameValid && lastNameValid && postalValid;
-    if (!valid) {
+    if (!isValid) {
       this.errorMessage.innerText = 'Please enter any missing contact information below';
     } else {
       this.errorMessage.innerText = '';
     }
 
-    return valid;
-  }
-
-  checkValidity(): boolean {
-    const emailValid = this.emailField.checkValidity();
-    const firstNameValid = this.firstNameField.checkValidity();
-    const lastNameValid = this.lastNameField.checkValidity();
-    const postalValid = this.postalCodeField.checkValidity();
-    return emailValid && firstNameValid && lastNameValid && postalValid;
+    return isValid;
   }
 
   focus(): void {
@@ -76,12 +87,12 @@ export class ContactForm extends LitElement {
   /** @inheritdoc */
   render(): TemplateResult {
     return html`
-      <div id="error-message"></div>
+      <div id="donation-contact-form-error-message"></div>
       <form>
         <fieldset>
-          <div class="row has-icon">
+          <div class="row">
             ${this.generateInput({
-              id: 'email',
+              id: 'donation-contact-form-email',
               placeholder: 'Email',
               required: true,
               fieldType: 'email',
@@ -91,9 +102,12 @@ export class ContactForm extends LitElement {
               icon: emailImg,
             })}
           </div>
-          <div class="row has-icon">
+        </fieldset>
+
+        <fieldset>
+          <div class="row">
             ${this.generateInput({
-              id: 'first-name',
+              id: 'donation-contact-form-first-name',
               placeholder: 'First name',
               name: 'fname',
               required: true,
@@ -101,32 +115,90 @@ export class ContactForm extends LitElement {
               autocomplete: 'given-name',
               icon: userIcon,
             })}
+          </div>
+          <div class="row">
             ${this.generateInput({
-              id: 'last-name',
+              id: 'donation-contact-form-last-name',
               placeholder: 'Last name',
               name: 'lname',
               autocomplete: 'family-name',
               required: true,
               maxlength: 255,
-              iconSpaceOption: IconSpaceOption.NoIconSpace,
             })}
           </div>
-          <div class="row has-icon">
+        </fieldset>
+        <fieldset>
+          <div class="row">
             ${this.generateInput({
-              id: 'postal-code',
+              id: 'donation-contact-form-street-address',
+              placeholder: 'Address Line 1',
+              required: true,
+              autocomplete: 'address-line1',
+              icon: localePinImg,
+              name: 'street-address',
+            })}
+          </div>
+          <div class="row">
+            ${this.generateInput({
+              id: 'donation-contact-form-extended-address',
+              placeholder: 'Address Line 2 (optional)',
+              autocomplete: 'address-line2',
+              required: false,
+              name: 'extended-address',
+            })}
+          </div>
+          <div class="row">
+            ${this.generateInput({
+              id: 'donation-contact-form-locality',
+              placeholder: 'City',
+              autocomplete: 'address-level2',
+              required: true,
+              name: 'locality',
+            })}
+          </div>
+          <div class="row">
+            ${this.generateInput({
+              id: 'donation-contact-form-region',
+              placeholder: 'State / Province',
+              autocomplete: 'address-level1',
+              required: true,
+              name: 'region',
+            })}
+            ${this.generateInput({
+              id: 'donation-contact-form-postal-code',
               placeholder: 'Zip / Postal',
               autocomplete: 'postal-code',
               required: true,
               name: 'postal',
-              icon: localePinImg,
               maxlength: 9,
               // must start with a character, then may contain spaces
               validationPattern: '[a-zA-Z\\-\\d]+[a-zA-Z\\-\\d\\s]*',
+              iconSpaceOption: SpacerOption.CompressSpace,
             })}
+          </div>
+          <div class="row">
+            ${this.countrySelectorTemplate}
           </div>
         </fieldset>
       </form>
       ${this.getStyles}
+    `;
+  }
+
+  private get countrySelectorTemplate(): TemplateResult {
+    const selectedCountry = 'US';
+
+    return html`
+      <badged-input>
+        <select id="donation-contact-form-countryCodeAlpha2">
+          ${Object.keys(countries).map(key => {
+            const name = countries[key];
+            return html`
+              <option value=${key} ?selected=${key === selectedCountry}>${name}</option>
+            `;
+          })}
+        </select>
+      </badged-input>
     `;
   }
 
@@ -157,12 +229,12 @@ export class ContactForm extends LitElement {
     maxlength?: number;
     name: string;
     icon?: TemplateResult;
-    iconSpaceOption?: IconSpaceOption;
+    iconSpaceOption?: SpacerOption;
     validationPattern?: string;
   }): TemplateResult {
     const required = options.required ?? true;
     const fieldType = options.fieldType ?? 'text';
-    const iconOption = options.iconSpaceOption ?? IconSpaceOption.IconSpace;
+    const iconOption = options.iconSpaceOption ?? SpacerOption.LeaveSpace;
 
     return html`
       <badged-input
@@ -175,7 +247,7 @@ export class ContactForm extends LitElement {
         <input
           type=${fieldType}
           id=${options.id}
-          class="contact-form-input"
+          class="donation-contact-form-input"
           name=${options.name}
           aria-label=${options.placeholder}
           placeholder=${options.placeholder}
@@ -198,7 +270,12 @@ export class ContactForm extends LitElement {
 
   get billingInfo(): BillingInfo {
     const billingInfo = new BillingInfo({
+      streetAddress: this.streetAddressField.value,
+      extendedAddress: this.extendedAddressField.value,
+      locality: this.localityField.value,
+      region: this.regionField.value,
       postalCode: this.postalCodeField.value,
+      countryCodeAlpha2: this.countryCodeAlpha2Field.value,
     });
     return billingInfo;
   }
@@ -222,7 +299,7 @@ export class ContactForm extends LitElement {
    * @readonly
    * @private
    * @type {TemplateResult}
-   * @memberof IADonationFormController
+   * @memberof ContactForm
    */
   private get getStyles(): TemplateResult {
     const noIconSpacerWidth = css`var(--badgedInputNoIconSpacerWidth, 3rem)`;
@@ -238,6 +315,11 @@ export class ContactForm extends LitElement {
 
     return html`
       <style>
+        /*
+          **NOTE**
+          This element is in the lightDOM so be sure to prefix all styles
+          with "contact-form" so styles don't leak.
+         */
         contact-form fieldset {
           border: 0;
           padding: 0;
@@ -256,29 +338,43 @@ export class ContactForm extends LitElement {
           margin-top: 0;
         }
 
-        contact-form badged-input {
-          width: 100%;
+        contact-form badged-input.donation-contact-form-region {
+          width: 60%;
         }
 
-        contact-form badged-input.last-name {
-          margin-left: -1px;
+        contact-form badged-input.donation-contact-form-postal-code {
+          width: 40%;
         }
 
-        contact-form #error-message {
+        contact-form #donation-contact-form-region {
+          width: ${iconFieldWidth};
+        }
+
+        contact-form #donation-contact-form-postal-code {
+          width: ${noIconFieldWidth};
+        }
+
+        contact-form #donation-contact-form-error-message {
           color: red;
           font-size: 1.4rem;
           margin-bottom: 0.6rem;
         }
 
-        contact-form #last-name {
+        contact-form #donation-contact-form-last-name {
           width: ${noIconFieldWidth};
         }
 
+        /* only show for screen readers */
         contact-form label {
-          display: none;
+          position: absolute;
+          left: -10000px;
+          top: auto;
+          width: 1px;
+          height: 1px;
+          overflow: hidden;
         }
 
-        contact-form .contact-form-input {
+        contact-form .donation-contact-form-input {
           width: ${iconFieldWidth};
           border: 0;
           outline: 0;
@@ -290,8 +386,22 @@ export class ContactForm extends LitElement {
           font-family: ${fieldFontFamily};
         }
 
-        contact-form .contact-form-input::placeholder {
+        contact-form .donation-contact-form-input::placeholder {
           color: revert;
+        }
+
+        contact-form #donation-contact-form-countryCodeAlpha2 {
+          width: ${iconFieldWidth};
+          height: 100%;
+          box-sizing: border-box;
+          font-weight: bold;
+          font-size: ${fieldFontSize};
+          font-family: ${fieldFontFamily};
+          border: 0;
+          background: #fff;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
         }
       </style>
     `;
