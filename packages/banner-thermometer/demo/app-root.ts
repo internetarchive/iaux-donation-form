16b44cee9cb5fca115767c58e39d6ebeceaa7c21
Jason Buckner <jasonb@archive.org>
2021-11-18 12:24:53 -0800

    WEBDEV-4807: Update banner thermometer design (#83)
    * Convert demo app to use app-root
    
    * Update thermometer design and unit tests
    
    * Always round to millions
    
    * Add css var for goal text color
    
    * Update resize observer dependency
    
    * Bump banner-thermometer to 0.4.0-alpha.1
    
    * Undo tsconfig change
    
    * Fix import
    
    * Bump shared resize observer to latest

---
+++ packages/banner-thermometer/demo/app-root.ts
@@ -0,0 +1,236 @@
+import { LitElement, html, TemplateResult, CSSResultGroup, css } from 'lit';
+import { customElement, query, state } from 'lit/decorators.js';
+
+import '../src/banner-thermometer';
+import {
+  DonationBannerThermometer,
+  GoalMessageMode,
+} from '../src/banner-thermometer';
+import { SharedResizeObserver } from '@internetarchive/shared-resize-observer';
+
+@customElement('app-root')
+export class AppRoot extends LitElement {
+  @query('#thermometer') thermometer!: DonationBannerThermometer;
+
+  @query('#thermometerHeightValue') thermometerHeightValue!: HTMLSpanElement;
+
+  @query('#currentAmountSlider') currentAmountSlider!: HTMLInputElement;
+
+  @query('#currentAmountInput') currentAmountInput!: HTMLInputElement;
+
+  @query('#goalInput') goalInput!: HTMLInputElement;
+
+  @state() defaultGoal = 6_500_000;
+
+  @state() defaultCurrentAmount = 2_350_000;
+
+  private resizeObserver = new SharedResizeObserver();
+
+  render(): TemplateResult {
+    return html`
+      <div class="thermometer-container">
+        <donation-banner-thermometer
+          id="thermometer"
+          .currentAmount=${this.defaultCurrentAmount}
+          .goalAmount=${this.defaultGoal}
+          .resizeObserver=${this.resizeObserver}
+        >
+        </donation-banner-thermometer>
+      </div>
+
+      <fieldset>
+        <legend>Config</legend>
+        <dl>
+          <dt><label for="goalInput">Set Goal</label></dt>
+          <dd>
+            <input
+              type="text"
+              id="goalInput"
+              @input=${this.setGoal}
+              value=${this.defaultGoal}
+            />
+          </dd>
+
+          <dt><label for="setGoal">Set Current Amount</label></dt>
+          <dd>
+            <input
+              type="text"
+              id="currentAmountInput"
+              @input=${this.setCurrentAmount}
+              value=${this.defaultCurrentAmount}
+            />
+            <input
+              type="range"
+              id="currentAmountSlider"
+              @input=${this.setCurrentAmountSlider}
+              min="0"
+              max="${this.defaultGoal}"
+            />
+          </dd>
+
+          <dt><label for="goalMet">Goal Message</label></dt>
+          <dd>
+            <div>
+              <input type="checkbox" id="goalMet" @input=${this.goalMet} />
+            </div>
+            <div>
+              <label>Goal Met Message:</label>
+              <input
+                type="text"
+                id="goalMetMessage"
+                @input=${this.goalMetMessage}
+                value="We've reached our goal!"
+              />
+            </div>
+            <div>
+              <label>Goal Near Message:</label>
+              <input
+                type="text"
+                id="goalNearMessage"
+                @input=${this.goalNearMessage}
+                value="We’ve almost reached our goal!"
+              />
+            </div>
+          </dd>
+
+          <dt><label for="borderColor">Thermometer Border</label></dt>
+          <dd>
+            <input
+              type="text"
+              id="borderColor"
+              @input=${this.changeBorderColor}
+              value="1px solid #31A481"
+            />
+          </dd>
+
+          <dt><label for="backgroundColor">Background Color</label></dt>
+          <dd>
+            <input
+              type="text"
+              id="backgroundColor"
+              @input=${this.changeBackgroundColor}
+              value="#D1FAED"
+            />
+          </dd>
+
+          <dt><label for="fillColor">Fill Color</label></dt>
+          <dd>
+            <input
+              type="text"
+              id="fillColor"
+              @input=${this.changeFillColor}
+              value="#31A481"
+            />
+          </dd>
+
+          <dt><label for="thermometerHeight">Thermometer Height</label></dt>
+          <dd>
+            <input
+              type="range"
+              id="thermometerHeight"
+              @input=${this.changeThermometerHeight}
+              min="10"
+              max="100"
+              value="20"
+            />
+            <span id="thermometerHeightValue">20px</span>
+          </dd>
+        </dl>
+      </fieldset>
+    `;
+  }
+
+  changeThermometerHeight(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.style.height = value + 'px';
+    const numberValue = parseFloat(value);
+    this.thermometerHeightValue.innerHTML = `${Math.round(numberValue)}px`;
+  }
+
+  goalMet(e: Event): void {
+    if (!e.target) return;
+    const target = e.target as HTMLInputElement;
+    const mode = target.checked
+      ? GoalMessageMode.ShowGoalMessage
+      : GoalMessageMode.ShowGoalAmount;
+    this.thermometer.goalMessageMode = mode;
+  }
+
+  setGoal(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.currentAmountSlider.max = value;
+    this.thermometer.goalAmount = parseFloat(value);
+  }
+
+  setCurrentAmount(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.currentAmount = parseFloat(value);
+  }
+
+  setCurrentAmountSlider(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.currentAmountInput.value = value;
+    this.thermometer.currentAmount = parseFloat(value);
+  }
+
+  goalMetMessage(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.goalReachedMessage = value;
+  }
+
+  goalNearMessage(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.goalNearMessage = value;
+  }
+
+  changeBorderColor(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.style.setProperty('--bannerThermometerBorder', value);
+  }
+
+  changeFillColor(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.style.setProperty(
+      '--bannerThermometerProgressColor',
+      value
+    );
+  }
+
+  changeBackgroundColor(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.style.setProperty(
+      '--bannerThermometerBackgroundColor',
+      value
+    );
+  }
+
+  changeMarkerBorder(e: Event): void {
+    if (!e.target) return;
+    const value = (e.target as HTMLInputElement).value;
+    this.thermometer.style.setProperty(
+      '--bannerThermometerMarkerBorder',
+      value
+    );
+  }
+
+  static get styles(): CSSResultGroup {
+    return css`
+      #thermometer {
+        height: 20px;
+      }
+
+      .thermometer-container {
+        margin-bottom: 2rem;
+      }
+    `;
+  }
+}
--- packages/banner-thermometer/test/banner-thermometer.test.ts
+++ packages/banner-thermometer/test/banner-thermometer.test.ts
@@ -6,6 +6,8 @@ import {
 } from '../src/banner-thermometer';
 import '../src/banner-thermometer';
 
+import { MockSharedResizeObserver } from '@internetarchive/shared-resize-observer/dist/test/mock-shared-resize-observer';
+
 describe('DonationBannerThermometer', () => {
   it('has a background track and fill layer', async () => {
     const el: DonationBannerThermometer = await fixture(html`
@@ -29,7 +31,7 @@ describe('DonationBannerThermometer', () => {
     const goalMessage = el.shadowRoot?.querySelector(
       '.donate-goal'
     ) as HTMLDivElement;
-    expect(goalMessage.innerText).to.equal('$1,000,000 GOAL');
+    expect(goalMessage.innerText).to.equal('$1MM GOAL');
   });
 
   it('can display goal met message', async () => {
@@ -64,5 +66,161 @@ describe('DonationBannerThermometer', () => {
       '.donate-goal'
     ) as HTMLDivElement;
     expect(goalMessage.innerText).to.equal('GOAL NEAR');
+  });
+
+  it('shows the current value on the right if the fill is too skinny', async () => {
+    const el: DonationBannerThermometer = await fixture(html`
+      <donation-banner-thermometer
+        .goalAmount=${1_000_000}
+        .currentAmount=${1_000}
+      ></donation-banner-thermometer>
+    `);
+
+    const thermometerValue = el.shadowRoot?.querySelector(
+      '.thermometer-value'
+    ) as HTMLDivElement;
+    const thermometerFill = el.shadowRoot?.querySelector(
+      '.thermometer-fill'
+    ) as HTMLDivElement;
+
+    const sizes = new Map<Element, DOMRectReadOnly>();
+    sizes.set(thermometerValue, {
+      bottom: 0,
+      height: 20,
+      left: 0,
+      right: 0,
+      top: 0,
+      width: 150,
+      x: 0,
+      y: 0,
+      toJSON: () => ({}),
+    });
+    sizes.set(thermometerFill, {
+      bottom: 0,
+      height: 20,
+      left: 0,
+      right: 0,
+      top: 0,
+      width: 100,
+      x: 0,
+      y: 0,
+      toJSON: () => ({}),
+    });
+
+    await new Promise<void>(resolve => {
+      const mockResizeObserver = new MockSharedResizeObserver({
+        targetSizes: sizes,
+        addObserverComplete: (): void => {
+          resolve();
+        },
+      });
+
+      el.resizeObserver = mockResizeObserver;
+    });
+
+    await el.updateComplete;
+    const thermometerBackground = el.shadowRoot?.querySelector(
+      '.thermometer-background'
+    ) as HTMLDivElement;
+    expect(thermometerBackground.classList.contains('value-right')).to.be.true;
+  });
+
+  it('shows the current value on the left if there is room', async () => {
+    const el: DonationBannerThermometer = await fixture(html`
+      <donation-banner-thermometer
+        .goalAmount=${1_000_000}
+        .currentAmount=${750_000}
+      ></donation-banner-thermometer>
+    `);
+
+    const thermometerValue = el.shadowRoot?.querySelector(
+      '.thermometer-value'
+    ) as HTMLDivElement;
+    const thermometerFill = el.shadowRoot?.querySelector(
+      '.thermometer-fill'
+    ) as HTMLDivElement;
+
+    const sizes = new Map<Element, DOMRectReadOnly>();
+    sizes.set(thermometerValue, {
+      bottom: 0,
+      height: 20,
+      left: 0,
+      right: 0,
+      top: 0,
+      width: 50,
+      x: 0,
+      y: 0,
+      toJSON: () => ({}),
+    });
+    sizes.set(thermometerFill, {
+      bottom: 0,
+      height: 20,
+      left: 0,
+      right: 0,
+      top: 0,
+      width: 100,
+      x: 0,
+      y: 0,
+      toJSON: () => ({}),
+    });
+
+    await new Promise<void>(resolve => {
+      const mockResizeObserver = new MockSharedResizeObserver({
+        targetSizes: sizes,
+        addObserverComplete: (): void => {
+          resolve();
+        },
+      });
+
+      el.resizeObserver = mockResizeObserver;
+    });
+
+    await el.updateComplete;
+    const thermometerBackground = el.shadowRoot?.querySelector(
+      '.thermometer-background'
+    ) as HTMLDivElement;
+    expect(thermometerBackground.classList.contains('value-left')).to.be.true;
+  });
+
+  it('sets the height of the banner on resize', async () => {
+    const el: DonationBannerThermometer = await fixture(html`
+      <donation-banner-thermometer
+        .goalAmount=${1_000_000}
+        .currentAmount=${1_000}
+      ></donation-banner-thermometer>
+    `);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const host = el.shadowRoot!.host;
+
+    const sizes = new Map<Element, DOMRectReadOnly>();
+    sizes.set(host, {
+      bottom: 0,
+      height: 35,
+      left: 0,
+      right: 0,
+      top: 0,
+      width: 150,
+      x: 0,
+      y: 0,
+      toJSON: () => ({}),
+    });
+
+    await new Promise<void>(resolve => {
+      const mockResizeObserver = new MockSharedResizeObserver({
+        targetSizes: sizes,
+        addObserverComplete: (): void => {
+          resolve();
+        },
+      });
+
+      el.resizeObserver = mockResizeObserver;
+    });
+
+    await el.updateComplete;
+    const styleProperty = el.style.getPropertyValue(
+      '--bannerThermometerHeight'
+    );
+    expect(styleProperty).to.equal('35px');
   });
 });
