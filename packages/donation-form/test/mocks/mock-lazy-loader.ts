9111cd00c742e721530f775baa914232b1a6d399
Jason Buckner <jason@jasonbuckner.com>
2021-12-01 09:54:20 -0800

    Update to lazy loader 0.2.0 with retry support

--- packages/donation-form/test/mocks/mock-lazy-loader.ts
+++ packages/donation-form/test/mocks/mock-lazy-loader.ts
@@ -1,19 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { LazyLoaderServiceInterface, BundleType } from '@internetarchive/lazy-loader-service';
+import {
+  LazyLoaderServiceEvents,
+  LazyLoaderServiceInterface,
+  BundleType,
+} from '@internetarchive/lazy-loader-service';
+import { createNanoEvents, Unsubscribe } from 'nanoevents';
 
 export class MockLazyLoader implements LazyLoaderServiceInterface {
+  private emitter = createNanoEvents<LazyLoaderServiceEvents>();
+
+  on<E extends keyof LazyLoaderServiceEvents>(
+    event: E,
+    callback: LazyLoaderServiceEvents[E],
+  ): Unsubscribe {
+    return this.emitter.on(event, callback);
+  }
+
   async loadBundle(bundle: {
     module?: string | undefined;
     nomodule?: string | undefined;
-  }): Promise<Event | undefined> {
-    return new Event('done');
+  }): Promise<void> {
+    return;
   }
+
   async loadScript(options: {
     src: string;
     bundleType?: BundleType | undefined;
-    attributes?: { key: string; value: any }[] | undefined;
-  }): Promise<Event> {
-    return new Event('done');
+    attributes?: Record<string, string> | undefined;
+  }): Promise<void> {
+    return;
   }
+  // async loadBundle(bundle: {
+  //   module?: string | undefined;
+  //   nomodule?: string | undefined;
+  // }): Promise<Event | undefined> {
+  //   return new Event('done');
+  // }
+  // async loadScript(options: {
+  //   src: string;
+  //   bundleType?: BundleType | undefined;
+  //   attributes?: { key: string; value: any }[] | undefined;
+  // }): Promise<Event> {
+  //   return new Event('done');
+  // }
 }
