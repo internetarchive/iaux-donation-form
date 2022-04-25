b9f68405fef157d9ef633de6f0ac31ac6b35c23f
Jason Buckner <jason@jasonbuckner.com>
2022-04-25 13:47:06 -0700

    Fix PayPal checkout UI (#120)
    * Switch payal enums to types
    
    * Use intent: capture during paypal flow to restore UI
    
    * Bump to 0.5.11-alpha.2
    
    * Bump to 0.5.11

--- packages/donation-form/src/@types/paypal-checkout-components/modules/callback-data.d.ts
+++ packages/donation-form/src/@types/paypal-checkout-components/modules/callback-data.d.ts
@@ -1,7 +1,4 @@
-export enum LineItemKind {
-  Debit = 'debit',
-  Credit = 'credit',
-}
+export type LineItemKind = 'debit' | 'credit';
 
 export interface LineItem {
   /**
@@ -45,17 +42,7 @@ export interface LineItem {
   url: string | undefined;
 }
 
-export enum ShippingOptionType {
-  /**
-   * The payer intends to receive the items at a specified address.
-   */
-  Shipping = 'SHIPPING',
-
-  /**
-   * The payer intends to pick up the items at a specified address. For example, a store address.
-   */
-  Pickup = 'PICKUP',
-}
+export type ShippingOptionType = 'SHIPPING' | 'PICKUP';
 
 export interface CurrencyAmount {
   /**
@@ -219,37 +206,9 @@ export interface TokenizePayload {
   details: TokenizePayloadDetails;
 }
 
-export enum FlowType {
-  /**
-   * Used to store the payment method for future use, ie subscriptions
-   */
-  Vault = 'vault',
+export type FlowType = 'checkout' | 'vault';
 
-  /**
-   * Used for one-time checkout
-   */
-  Checkout = 'checkout',
-}
-
-export enum Intent {
-  /**
-   * Submits the transaction for authorization but not settlement.
-   */
-  Authorize = 'authorize',
-
-  /**
-   * Validates the transaction without an authorization (i.e. without holding funds).
-   * Useful for authorizing and capturing funds up to 90 days after the order has been placed.
-   * Only available for Checkout flow.
-   */
-  Order = 'order',
-
-  /**
-   * Payment will be immediately submitted for settlement upon creating a transaction.
-   * `sale` can be used as an alias for this value.
-   */
-  Capture = 'capture',
-}
+export type Intent = 'authorize' | 'order' | 'capture';
 
 export interface AuthorizationData {
   payerId: string;
