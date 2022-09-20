917e8e12ef1832a3b9bf45f5a501bab74a22565f
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-09-20 10:55:02 -0700

    add no sampling type

--- packages/donation-form/src/@types/analytics-handler/index.d.ts
+++ packages/donation-form/src/@types/analytics-handler/index.d.ts
@@ -18,4 +18,20 @@ export interface AnalyticsHandlerInterface {
     label?: string,
     additionalEventParams?: object,
   ): void;
+
+  /**
+   * Sends a tracking event to no sampling bucket.
+   *
+   * @param {string} category
+   * @param {string} action
+   * @param {string} label
+   * @param {Object} additionalEventParams These are internal params, not sent to GA
+   */
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  send_event_no_sampling(
+    category: string,
+    action: string,
+    label?: string,
+    additionalEventParams?: object,
+  ): void;
 }
