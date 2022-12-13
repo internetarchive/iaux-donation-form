25bc7110763506e15571c525bc22aa4e238737f8
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-12-13 15:01:46 -0800

    update default to $10 - WEBDEV-5672

--- packages/data-models/src/defaults/default-donation-amounts.ts
+++ packages/data-models/src/defaults/default-donation-amounts.ts
@@ -1,20 +1,14 @@
-import { DonationPaymentInfo } from "../donation-info/donation-payment-info"
-import { DonationType } from "../donation-info/donation-type";
+import { DonationPaymentInfo } from '../donation-info/donation-payment-info';
+import { DonationType } from '../donation-info/donation-type';
 
 // the default donation amounts to show in the donation amount selector
-export const defaultDonationAmounts: number[] = [
-  5,
-  10,
-  25,
-  50,
-  100,
-  500,
-  1000,
-]
+export const defaultDonationAmounts: number[] = [5, 10, 25, 50, 100, 500, 1000];
 
 // the default amount selected
-export const defaultSelectedDonationInfo: DonationPaymentInfo = new DonationPaymentInfo({
-  donationType: DonationType.OneTime,
-  amount: 5,
-  coverFees: false,
-});
+export const defaultSelectedDonationInfo: DonationPaymentInfo = new DonationPaymentInfo(
+  {
+    donationType: DonationType.OneTime,
+    amount: 10,
+    coverFees: false,
+  }
+);
