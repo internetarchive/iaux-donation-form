70a71992dca8ab1b3f4d3278f9d6ac347af2f934
Jason Buckner <jason@jasonbuckner.com>
2024-12-23 10:59:35 -0800

    Fix tests

--- packages/donation-form/test/tests/models/donation-payment-info.test.ts
+++ packages/donation-form/test/tests/models/donation-payment-info.test.ts
@@ -4,7 +4,7 @@ import { DonationPaymentInfo, DonationType } from '@internetarchive/donation-for
 // this is a separate calculation from the one in DonationPaymentInfo
 // to have a second verification independent of it
 function calculateFee(amount: number): number {
-  return Math.round((amount * 0.022 + 0.3) * 100) / 100;
+  return Math.round((amount * 0.02 + 0.49) * 100) / 100;
 }
 
 describe('Donation Payment Info', () => {
@@ -21,7 +21,7 @@ describe('Donation Payment Info', () => {
   it('calculates the proper total for fees coverage', async () => {
     const baseAmount = 3.5;
     const expectedFee = calculateFee(baseAmount);
-    const exptectedTotal = baseAmount + expectedFee;
+    const expectedTotal = Math.round((baseAmount + expectedFee) * 100) / 100;
 
     const donationInfo = new DonationPaymentInfo({
       donationType: DonationType.OneTime,
@@ -29,7 +29,7 @@ describe('Donation Payment Info', () => {
       coverFees: true,
     });
 
-    expect(donationInfo.total).to.equal(exptectedTotal);
+    expect(donationInfo.total).to.equal(expectedTotal);
   });
 
   it('calculates the proper expected fees from manual calculations', async () => {
@@ -38,12 +38,12 @@ describe('Donation Payment Info', () => {
       amount: 5,
       coverFees: true,
     });
-    expect(donationInfo.fee).to.equal(0.41);
-    expect(donationInfo.total).to.equal(5.41);
+    expect(donationInfo.fee).to.equal(0.59);
+    expect(donationInfo.total).to.equal(5.59);
 
     donationInfo.amount = 3.5;
-    expect(donationInfo.fee).to.equal(0.38);
-    expect(donationInfo.total).to.equal(3.88);
+    expect(donationInfo.fee).to.equal(0.56);
+    expect(donationInfo.total).to.equal(4.06);
   });
 
   it('calculates the proper expected fees', async () => {
