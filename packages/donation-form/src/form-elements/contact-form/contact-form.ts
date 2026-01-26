88bcb81bb9d46466f3c0d5e2d0784f38d65aea5d
Jason Buckner <jason@jasonbuckner.com>
2026-01-26 13:52:14 -0800

    Linting

--- packages/donation-form/src/form-elements/contact-form/contact-form.ts
+++ packages/donation-form/src/form-elements/contact-form/contact-form.ts
@@ -74,15 +74,15 @@ export class ContactForm extends LitElement {
       badgedInput: BadgedInput;
       inputField: HTMLInputElement;
     }[] = [
-        { badgedInput: this.emailBadgedInput, inputField: this.emailField },
-        { badgedInput: this.firstNameBadgedInput, inputField: this.firstNameField },
-        { badgedInput: this.lastNameBadgedInput, inputField: this.lastNameField },
-        { badgedInput: this.streetAddressBadgedInput, inputField: this.streetAddressField },
-        { badgedInput: this.extendedAddressBadgedInput, inputField: this.extendedAddressField },
-        { badgedInput: this.localityBadgedInput, inputField: this.localityField },
-        { badgedInput: this.regionBadgedInput, inputField: this.regionField },
-        { badgedInput: this.postalBadgedInput, inputField: this.postalCodeField },
-      ];
+      { badgedInput: this.emailBadgedInput, inputField: this.emailField },
+      { badgedInput: this.firstNameBadgedInput, inputField: this.firstNameField },
+      { badgedInput: this.lastNameBadgedInput, inputField: this.lastNameField },
+      { badgedInput: this.streetAddressBadgedInput, inputField: this.streetAddressField },
+      { badgedInput: this.extendedAddressBadgedInput, inputField: this.extendedAddressField },
+      { badgedInput: this.localityBadgedInput, inputField: this.localityField },
+      { badgedInput: this.regionBadgedInput, inputField: this.regionField },
+      { badgedInput: this.postalBadgedInput, inputField: this.postalCodeField },
+    ];
     fields.forEach(({ badgedInput, inputField }) => {
       badgedInput.error = !inputField.checkValidity();
     });
